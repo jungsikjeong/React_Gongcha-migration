f856bff2d1ee0d7dc1baa0dfe633e1a4e9eaac04
JungSik Jeong <wndtlr1024@gmail.com>
2024-04-07 02:16:39 +0900

    Revise: 사용하지않는 코드 제거

--- client/src/hook/auth/use-user.tsx
+++ client/src/hook/auth/use-user.tsx
@@ -1,16 +1,10 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import instance from 'api/instance';
-import { authModalState } from 'atom/auth-modal-atoms';
 import { IUserInfo } from 'interface/auth';
 import { useEffect } from 'react';
 import { userKey } from 'react-query-key/auth.keys';
-import { SetterOrUpdater, useSetRecoilState } from 'recoil';
 
-interface IFetchUserInfo {
-  setAuthModalState: SetterOrUpdater<boolean>;
-}
-
-const fetchUserInfo = async ({ setAuthModalState }: IFetchUserInfo) => {
+const fetchUserInfo = async () => {
   const res = await instance.get('/api/auth');
 
   return res.data;
@@ -21,12 +15,11 @@ interface IUseUser {
 }
 
 export function useUser(): IUseUser {
-  const setAuthModalState = useSetRecoilState(authModalState);
   const queryClient = useQueryClient();
 
   const { data: user } = useQuery({
     queryKey: [userKey.user],
-    queryFn: async () => await fetchUserInfo({ setAuthModalState }),
+    queryFn: async () => await fetchUserInfo(),
     staleTime: 1000 * 60 * 10,
     gcTime: 1000 * 60 * 10,
   });
