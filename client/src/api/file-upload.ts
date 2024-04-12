8e6a148cf5292437ef0d287a6e7ad664a5129f3f
JungSik Jeong <wndtlr1024@gmail.com>
2024-04-13 00:17:20 +0900

    Revise: 파일업로드 api 변경

---
+++ client/src/api/file-upload.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { getToken } from 'utils/token.localstorage';
+
+export const postFileUpload = async ({ formData }: { formData: FormData }) => {
+  try {
+    const token = getToken();
+
+    const res = await axios.post<string[]>('/api/posts/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    return res.data;
+  } catch (error: any) {
+    if (error?.response?.status === 401) {
+      toast.error('업로드중 에러가 발생했습니다. 다시 시도해주세요');
+      window.location.reload();
+    }
+  }
+};
