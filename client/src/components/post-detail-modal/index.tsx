81d00b02143b871150f4414d96e31ce2a8dda11c
JungSik Jeong <wndtlr1024@gmail.com>
2024-04-16 15:50:22 +0900

    Styles: 모달창 밖 영역 클릭시 close, 및 모달창 애니메이션 변경

--- client/src/components/post-detail-modal/index.tsx
+++ client/src/components/post-detail-modal/index.tsx
@@ -1,8 +1,7 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { IoMdClose } from 'react-icons/io';
-import { useSetRecoilState } from 'recoil';
-import styled from 'styled-components';
+import { useRecoilState } from 'recoil';
+import styled, { keyframes } from 'styled-components';
 
 import { postDetailModalStatus } from '../../atom/post-detail-modal-atoms';
 import UseFetchPostDetail from './hook/use-fetch-post-detail';
@@ -10,7 +9,23 @@ import UseFetchPostDetail from './hook/use-fetch-post-detail';
 import PostDetailContentsMobile from './post-detail-contents/post-detail-contents-mobile';
 import PostDetailContentsPC from './post-detail-contents/post-detail-contents-pc';
 
-const Container = styled(motion.div)`
+const fadeInAnimation = keyframes`
+  0% {
+    opacity: 0;
+    opacity: 0;
+     scale: 0;
+  }
+  50% {
+    opacity: 1;
+    scale: 1.1;
+  }
+  100%{
+    
+    scale: 1;
+  }
+`;
+
+const Container = styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -25,6 +40,7 @@ const Container = styled(motion.div)`
   background-color: rgba(0, 0, 0, 0.5);
   z-index: 20;
   overflow: hidden;
+  animation: ${fadeInAnimation} 0.3s ease-in-out;
 
   @media (max-width: 768px) {
     align-items: start;
@@ -51,6 +67,7 @@ const Close = styled.div`
   position: absolute;
   right: 30px;
   font-size: 2rem;
+  color: #fff;
   cursor: pointer;
   @media (max-width: 768px) {
     display: none;
@@ -65,29 +82,16 @@ const PostDetailModal = ({ postId }: IPostDetailModal) => {
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [boxHeight, setBoxHeight] = useState(window.innerWidth / 1.5);
 
-  const setPostDetailModal = useSetRecoilState(postDetailModalStatus);
+  const [postDetailModal, setPostDetailModal] = useRecoilState(
+    postDetailModalStatus
+  );
 
   const { data, isLoading } = UseFetchPostDetail(postId);
 
+  const divRef = useRef<HTMLDivElement>(null);
+
   useEffect(() => {
     return () => setPostDetailModal(false);
-  }, []);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth <= 768) {
-        setIsMobile(true);
-      } else {
-        setBoxHeight(window.innerWidth / 1.875);
-        setIsMobile(false);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
   }, []);
 
   useEffect(() => {
@@ -99,36 +103,55 @@ const PostDetailModal = ({ postId }: IPostDetailModal) => {
         setPostDetailModal(false);
       }
     };
+    const handleOutsideClose = (e: MouseEvent) => {
+      if (
+        divRef.current &&
+        !divRef.current.contains(e.target as Node) &&
+        postDetailModal
+      ) {
+        setPostDetailModal(false);
+      }
+    };
 
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsMobile(true);
+      } else {
+        setBoxHeight(window.innerWidth / 1.875);
+        setIsMobile(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    document.addEventListener('mousedown', handleOutsideClose);
     document.addEventListener('keydown', handleKeyPress);
 
     return () => {
       // 모달이 닫힐 때 body에 추가한 overflow: hidden 스타일 제거
       document.body.style.overflow = '';
+      document.removeEventListener('mousedown', handleOutsideClose);
       document.removeEventListener('keydown', handleKeyPress);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   return (
-    <AnimatePresence>
-      <Container>
-        <Wrapper $boxheight={boxHeight}>
-          <Close
-            onClick={() => {
-              setPostDetailModal(false);
-            }}
-          >
-            <IoMdClose />
-          </Close>
-
-          {isMobile ? (
-            <PostDetailContentsMobile post={data} />
-          ) : (
-            <PostDetailContentsPC post={data} />
-          )}
-        </Wrapper>
-      </Container>
-    </AnimatePresence>
+    <Container>
+      <Wrapper $boxheight={boxHeight} ref={divRef}>
+        <Close
+          onClick={() => {
+            setPostDetailModal(false);
+          }}
+        >
+          <IoMdClose />
+        </Close>
+        {isMobile ? (
+          <PostDetailContentsMobile post={data} />
+        ) : (
+          <PostDetailContentsPC post={data} />
+        )}
+      </Wrapper>
+    </Container>
   );
 };
 
