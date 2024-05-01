e5ae730186702d04ed915c6e5b48cd690d0de5fe
JungSik Jeong <wndtlr1024@gmail.com>
2024-05-01 18:32:05 +0900

    Styles: 스켈레톤 css 변경

--- client/src/components/common/skeleton.tsx
+++ client/src/components/common/skeleton.tsx
@@ -3,10 +3,10 @@ import styled, { keyframes } from 'styled-components';
 // 스켈레톤 애니메이션
 const skeletonAnimation = keyframes`
   0% {
-    background-position: -200px 0;
+    background-position:100% 0%;
   }
   100% {
-    background-position: 200px 0;
+    background-position:0% 0%;
   }
   `;
 
@@ -20,9 +20,10 @@ const SkeletonElement = styled.div<ISkeletonProps>`
   height: ${({ height }) => height};
   width: ${({ width }) => width};
   border-radius: ${({ $borderradius }) => $borderradius};
-  background: linear-gradient(90deg, #1e1e1e 25%, #0a0a0a 50%, #151515 75%);
-  background-size: 200px 100%;
-  animation: ${skeletonAnimation} 1.5s infinite linear;
+  background: linear-gradient(90deg, #1e1e1e 35%, #0a0a0a 50%, #151515 65%);
+  /* background-size: 200px 100%; */
+  background-size: 300% auto;
+  animation: ${skeletonAnimation} 2s infinite linear;
 `;
 
 const Skeleton = ({ width, height, $borderradius }: ISkeletonProps) => {
