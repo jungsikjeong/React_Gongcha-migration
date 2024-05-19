e31fde2eaf2a9743235809f5bb916e1b21f56f3f
JungSik Jeong <wndtlr1024@gmail.com>
2024-05-19 15:51:26 +0900

    Revise: vercel 배포 cors 시도5

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,9 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:5000',
+      // target: 'http://localhost:5000',
+      target:
+        'https://port-0-react-gongcha-migration-ss7z32llwcg7ke3.sel5.cloudtype.app/',
       changeOrigin: true,
     })
   );
